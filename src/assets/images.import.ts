635f98192ea4224a121cd3b0326424b97f661134
Alex-B012 <rad15on2014@gmail.com>
2026-06-01 23:36:12 +0300

    ts version - part 1 v1.0.5

--- src/assets/images.import.ts
+++ src/assets/images.import.ts
@@ -1,16 +1,16 @@
-import scream_img from "./images/scream.webp";
-import hobbit_img from "./images/hobbit.webp";
-import alice_img from "./images/alice.webp";
-import chicago_img from "./images/chicago.webp";
-import corner_boys_img from "./images/corner_boys.webp";
-import spiderman_img from "./images/spiderman.webp";
-import first_date_img from "./images/first_date.webp";
-import a_moment_called_forever_img from "./images/a_moment_called_forever.webp";
-import more_graffiti_img from "./images/more_graffiti.webp";
-import movies_img from "./images/movies.webp";
-import mr_briggs_img from "./images/mr_briggs.webp";
-import young_age_img from "./images/young_age.webp";
-import astra_space_adventure_img from "./images/astra_space_adventure.webp";
+import scream_img from "./images/n_scream.webp";
+import hobbit_img from "./images/n_hobbit.webp";
+import alice_img from "./images/n_alice.webp";
+import chicago_img from "./images/n_chicago.webp";
+import corner_boys_img from "./images/n_corner_boys.webp";
+import spiderman_img from "./images/n_spiderman.webp";
+import first_date_img from "./images/n_first_date.webp";
+import a_moment_called_forever_img from "./images/n_a_moment_called_forever.webp";
+import more_graffiti_img from "./images/n_more_graffiti.webp";
+import movies_img from "./images/n_movies.webp";
+import mr_briggs_img from "./images/n_mr_briggs.webp";
+import young_age_img from "./images/n_young_age.webp";
+import astra_space_adventure_img from "./images/n_astra_space_adventure.webp";
 
 export const images = {
   scream_img,
